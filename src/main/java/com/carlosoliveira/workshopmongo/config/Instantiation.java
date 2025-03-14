4c436736cfcb3b9c85bbedf97a35caf178979c08
Carlos <carlos.oliveirajr@hotmail.com>
2025-03-14 09:41:50 -0300

    Author data projection with DTO

--- src/main/java/com/carlosoliveira/workshopmongo/config/Instantiation.java
+++ src/main/java/com/carlosoliveira/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.carlosoliveira.workshopmongo.domain.Post;
 import com.carlosoliveira.workshopmongo.domain.User;
+import com.carlosoliveira.workshopmongo.dto.AuthorDTO;
 import com.carlosoliveira.workshopmongo.repository.PostRepository;
 import com.carlosoliveira.workshopmongo.repository.UserRepository;
 
@@ -36,11 +37,12 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Gray", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("13/03/2025"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("12/03/2025"), "Bom dia", "acordei feliz hoje", maria);
-		
-		
 		userRepository.saveAll(Arrays.asList(maria,alex,bob));
+		
+		Post post1 = new Post(null, sdf.parse("13/03/2025"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("12/03/2025"), "Bom dia", "acordei feliz hoje",new AuthorDTO(maria));
+			
+	
 		postRepository.saveAll(Arrays.asList(post1,post2));
 	}
 
--- src/main/java/com/carlosoliveira/workshopmongo/services/UserService.java
+++ src/main/java/com/carlosoliveira/workshopmongo/services/UserService.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import com.carlosoliveira.workshopmongo.domain.User;
 import com.carlosoliveira.workshopmongo.dto.UserDTO;
