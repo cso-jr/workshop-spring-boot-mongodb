23c308675e8873618ef6c7c3ddabe518a1107d6a
Carlos <carlos.oliveirajr@hotmail.com>
2025-03-19 22:56:41 -0300

    Adding comments to the post

--- src/main/java/com/carlosoliveira/workshopmongo/config/Instantiation.java
+++ src/main/java/com/carlosoliveira/workshopmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.carlosoliveira.workshopmongo.domain.Post;
 import com.carlosoliveira.workshopmongo.domain.User;
 import com.carlosoliveira.workshopmongo.dto.AuthorDTO;
+import com.carlosoliveira.workshopmongo.dto.CommentDTO;
 import com.carlosoliveira.workshopmongo.repository.PostRepository;
 import com.carlosoliveira.workshopmongo.repository.UserRepository;
 
@@ -42,11 +43,21 @@ public class Instantiation implements CommandLineRunner{
 		Post post1 = new Post(null, sdf.parse("13/03/2025"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("12/03/2025"), "Bom dia", "acordei feliz hoje", new AuthorDTO(maria));
 			
-	
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("21/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1,c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
 		postRepository.saveAll(Arrays.asList(post1,post2));
 		
 		maria.getPosts().addAll(Arrays.asList(post1,post2));
 		userRepository.save(maria);
+		
+		
+		
+		
 	}
 
 	
--- src/main/java/com/carlosoliveira/workshopmongo/domain/Post.java
+++ src/main/java/com/carlosoliveira/workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.carlosoliveira.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.carlosoliveira.workshopmongo.dto.AuthorDTO;
+import com.carlosoliveira.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable{
@@ -19,6 +22,8 @@ public class Post implements Serializable{
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 		
@@ -83,6 +88,16 @@ public class Post implements Serializable{
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
 	}
 
 
