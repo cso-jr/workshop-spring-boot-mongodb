d4e31d84692b8f594c021ed680cca820700ef6a3
Carlos <carlos.oliveirajr@hotmail.com>
2025-03-21 18:07:13 -0300

    Simple query using query methods

--- src/main/java/com/carlosoliveira/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/carlosoliveira/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.carlosoliveira.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,6 @@ import com.carlosoliveira.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	List<Post> findByTitleContainingIgnoreCase(String text);
+	
 }
